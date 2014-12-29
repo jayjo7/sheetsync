6936cf5f0f2073b19d1020a1f2c550ce5d6eefe9
jayjo7 <jayjo7.hotmail.com>
2014-12-29 10:20:17 -0800

    Clean and uniqueid logic

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ var express = require('express'),
 var app = express();
 var collectionDriver;
 app.set('port', process.env.PORT || 3000);
+app.set('mongodb_url', process.MONGO_URL || "mongodb://fifthdonut:fifthdonut123@kahana.mongohq.com:10051/Websheets");
+app.set('sheet_uniqueid_column_name', process.SHEET_UNIQUEID_COLUMN_NAME || "WebId");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-  MongoClient.connect("mongodb://fifthdonut:fifthdonut123@kahana.mongohq.com:10051/Websheets", {native_parser:true}, function(err, db) 
+
+MongoClient.connect(app.get('mongodb_url'), {native_parser:true}, function(err, db) 
   	{
   		if(err)
   		{
@@ -25,49 +28,57 @@ app.use(bodyParser.urlencoded({extended: true}));
 
   	});
 
-app.get('/', function(req, res){
+app.get('/', function(req, res)
+	{
 
-res.send('Get is working');
+		res.send('Get is working');
 
-});
+	});
 
-app.put('/sheetSync', function(req, res){
-
-
-	//var jsonString = JSON.stringify(req.body);
-	var keyOut ="";
+app.put('/sheetSync', function(req, res)
+{
+	var result = true;
+	var failure=[];
 
 	for(var key in req.body)
 	{
-		keyOut = key;
-		console.log( "key = " + keyOut);
+		
+
+		console.log( "Working with the worksheet = " + key);
+
 		var data= req.body[key];
 
-		console.log( "data.length= " + data.length);
+		console.log( "Number of records received = " + data.length);
 		
 
 		for (i=0; i<data.length; i++)
 		{
-			for (var key in data[i])
-			{
-				//console.log("date[" + i+ "] = " +data[i].toString());
-				console.log("data key = " + key);
 
-		   }
-		   collectionDriver.upsert(keyOut, data[i],"PhoneNumber",function(err,docs) {
-          	if (err) { console.log(err); } 
-          else { console.log("upsert Success"); } 
-     });
+		   collectionDriver.upsert(key, data[i], app.get('sheet_uniqueid_column_name') ,function(err,docs) 
+		   {
+          		if (err) 
+          			{ 
+          				console.log(err); 
+          				result = false;
+          				failure.push(data[i][app.get('sheet_uniqueid_column_name')]);
+
+          			} 
+          		else 
+          			{ 
+          				console.log("upsert Success");
+          			} 
+     		});
 		}
 
 
 	}
+	if (result)
+	{
+		var resultObject ={"result": "Success"};
 
-//var key = req.body.keys();
-
-
-	//console.log("body = " + jsonString);
-	//res.send("key = " + keyOut);
+		res.send(resultObject);	
+	}
+	
 });
 
 app.listen(app.get('port'));
